4e6cb70c4ad188c84bd2d99b2a416d790d7faae1
IranildoConstantino09 <iranildojuniorjuninho@gmail.com>
2026-05-14 19:47:20 -0300

    Atividade

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Iranildo");
-Console.WriteLine("15");
+Console.WriteLine("Informe seu nome: ");
+string Nome = Console.ReadLine();
+
+Console.WriteLine("Informe a sua idade: ");
+string idadeTexto = Console.ReadLine();
+int idadeConvertida = int.Parse(idadeTexto);
+
+Console.WriteLine("Informe sua altura ");
+string alturaTexto = Console.ReadLine();
+double alturaConvertida = double.Parse(alturaTexto); 
+
+Console.WriteLine("Você possui CNH? S/N ");
+string cnhTexto = Console.ReadLine().ToUpper();
+bool cnhConvertida;
+
+Console.WriteLine("Meu nome é: " + Nome);
+Console.WriteLine("Minha idade é: " + idadeConvertida);
+Console.WriteLine("Minha altura é: " + alturaConvertida);
+
+if (cnhTexto == "S" ){
+    cnhConvertida = true;
+    Console.WriteLine("Você possui habilitação ");
+} 
+else {
+    cnhConvertida = false;
+    Console.WriteLine("Você não possui habilitação ");
+}
+
